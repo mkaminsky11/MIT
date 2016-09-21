8cbe71269f70b409996b02d542cbd2b771f4ddb3
Michael Kaminsky <mkaminsky11@gmail.com>
2016-09-20 21:34:04 -0400

    add saving and stuff

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # MIT
 
 ## 1. Display charts + draw on them
-+ any building, any floor
-+ draw points
-  + to outside
-  + node
-  + stairs (up/down/both)
-  + elevators (which floors)
-+ draw lines
-+ save drawings
++ any building, any floor (kinda)
++ draw points (done)
+  + to outside (todo)
+  + node (done)
+  + stairs (up/down/both) (todo)
+  + elevators (which floors) (todo)
++ draw lines (todo)
++ save drawings (normalize positions to 1000px width)
 
 ## 2. turn drawings into database
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ nunjucks.configure('views', {
 });
 
 var json = JSON.parse(fs.readFileSync("data/floor.json"));
+var nodeData = JSON.parse(fs.readFileSync("json/data.json"));
 
 app.get('/', function (req, res) {
   res.render('index.html', {
@@ -23,5 +24,80 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/data/:building/:floor', function(req,res){
+	var _data = {
+		items: {},
+		connections: []
+	};
+	if(nodeData[req.params.building] && nodeData[req.params.building][req.params.floor]){
+		_data = nodeData[req.params.building][req.params.floor];
+	}
+	res.send(JSON.stringify(_data));
+});
+
+app.post('/update', function(req, res){
+	var data = {
+		floor: req.body.floor,
+		building: req.body.building,
+		items: req.body.items,
+		connections: req.body.connections
+	};
+	if(nodeData[req.body.building]){
+		//all good, continue
+	}
+	else{
+		nodeData[req.body.building] = {};
+	}
+	nodeData[req.body.building][req.body.floor] = data;
+	fs.writeFile("json/data.json", JSON.stringify(nodeData), function (err) {
+		if(!err){
+			res.send({
+				err: true
+			});
+		}
+		else{
+			res.send({
+				err: false
+			});
+		}
+	});
+});
+
 app.listen(3000, function () {
 });
+
+function findPath(room1, room2){
+	room1 = room1.split("-");
+	room2 = room2.split("-");
+	var bld1 = room1[0];
+	var bld2 = room2[0];
+	room1 = room1[1]
+	room2 = room2[1]
+	var floor1 = 0
+	var floor2 = 0
+	for(var i = 0; i < room1.length; i++){
+		if(isNaN(room1.charAt(i)) === false){
+			floor1 = room1.charAt(i);
+			break;
+		}
+	}
+	for(var j = 0; j < room2.length; j++){
+		if(isNaN(room2.charAt(j)) === false){
+			floor2 = room2.charAt(j);
+			break;
+		}
+	}
+
+	var id1 = getId(bld1, floor1, room1);
+	var id2 = getId(bld2, floor2, room2);
+}
+
+function getId(building, floor, room){
+	var all_items = nodeData[building][floor].items;
+	for id in all_items{
+		if(all_items[id].name === "room"){
+			return id;
+		}
+	}
+	return null;
+}
